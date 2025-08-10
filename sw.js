d72497cfe85292e600b4808892b92c87ad303a4b
Ismaell80057 <falippouismael@icloud.com>
2025-08-10 16:51:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,29 @@
-const CACHE='anti-tabac-mvp-v1';
-const ASSETS=[
-  '/', '/index.html', '/app.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/apple-touch-icon.png',
-  '/favicon.png',
-  '/manifest.json'
+const CACHE = 'anti-tabac-mvp-v3';
+const ASSETS = [
+  './', './index.html', './app.html',
+  './css/style.css',
+  './js/app.js',
+  './apple-touch-icon.png',
+  './favicon.png',
+  './manifest.json'
 ];
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
